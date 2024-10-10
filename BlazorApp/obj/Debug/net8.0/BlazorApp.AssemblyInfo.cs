820c493245017bed74610b37025ff4ad6f2f29d6
rrsm8d <rrsm8d@umkc.edu>
2024-10-10 14:59:57 -0500

    Additional adjustments to Login styles

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df29876eacfc62f9368bc5abaa19fffb74826167")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e3f3fee2fb45b9b5a2e097a6ca885f049183b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

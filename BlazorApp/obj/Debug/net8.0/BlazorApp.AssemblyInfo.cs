1b7104648a8a0240242a75e7f04baaf69a7bd0b1
Gregory Linville <gll4kc@umsystem.edu>
2024-10-10 19:13:54 -0500

    Change Weather and Counter to Login Signup in Navmenu.

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820c493245017bed74610b37025ff4ad6f2f29d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6af985e16a50667a9d2d6f02081ecdd18b6614ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

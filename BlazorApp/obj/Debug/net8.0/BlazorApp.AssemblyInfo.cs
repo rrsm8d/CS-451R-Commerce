0bda20fe767a287b6ceb80e3e8df8be02d859734
Gregory Linville <gll4kc@umsystem.edu>
2024-10-24 00:13:35 -0500

    Transactin Boxes

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc413cee819fb51b57cd8f88046d06a03a07373f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e14268f7afafb7a9f465da9f918673a11ecf44f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

8129c58f9068fa534fa318b451ef107c3ac04d61
rrsm8d <rrsm8d@umkc.edu>
2024-11-25 17:34:08 -0600

    Context update, Goals graphics, migration update required

--- BlazorApp/Data/BlazorAppContext.cs
+++ BlazorApp/Data/BlazorAppContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using BlazorApp.Models;
+using BlazorApp.Components.Pages;
 
 namespace BlazorApp.Data
 {
@@ -26,5 +27,48 @@ namespace BlazorApp.Data
                 .IsUnique();
             base.OnModelCreating(modelBuilder);
         }
+
+        public override int SaveChanges()
+        {
+            HandleTransactionLogic();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            HandleTransactionLogic();
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void HandleTransactionLogic()
+        {
+            // Get all newly added transactions
+            var newTransactions = ChangeTracker.Entries<Transaction>()
+                .Where(e => e.State == EntityState.Added)
+                .Select(e => e.Entity);
+            if(newTransactions.Count() != 0)
+            {
+                foreach (var transaction in newTransactions)
+                {
+                    // Find goals tied to same accountId
+                    var relatedGoals = BudgetPlan.Where(g => g.AccountId == transaction.AccountId);
+                    if (relatedGoals.Count() != 0)
+                    {
+                        foreach(var goal in relatedGoals)
+                        {
+                            if((transaction.TransactionDate >= goal.StartDate) && (transaction.TransactionDate <= goal.EndDate))
+                            {
+                                // Increment the goal's TotalSpent
+                                goal.BudgetExpenditures += transaction.TransactionAmount;
+
+                                // Mark the goal as modified to save changes
+                                Entry(goal).State = EntityState.Modified;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
